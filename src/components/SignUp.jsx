5ae1dc31b4ed2762dc2d7baa3da003f3ca1838c1
RahulGang1 <rahul7055648508@gmail.com>
2024-10-17 11:07:13 +0530

    Add the files

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -74,7 +74,7 @@ const SignupPage = () => {
             showPassword={showConfirmPassword}
             setShowPassword={setShowConfirmPassword}
           />
-
+          
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full mt-4"
             type="submit"
