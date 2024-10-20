864fd0daa2860de09f6e71f202d5917b4a395078
RahulGang1 <rahul7055648508@gmail.com>
2024-10-20 14:33:06 +0530

    Add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './components/Login'; // import your LoginPage component (correct file path)
-import SignupPage from './components/SignUp'; // import your SignupPage component (correct file path)
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import LoginPage from './components/Login'; // import your LoginPage component (correct file path)
+// import SignupPage from './components/SignUp'; // import your SignupPage component (correct file path)
 import Navbar from './components/Navbar'; // import your Navbar component
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Router>
+ 
       <Navbar />
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
-    <Contact/>
-    <Footer/>
-    </Router>
+      
   );
 };
 
